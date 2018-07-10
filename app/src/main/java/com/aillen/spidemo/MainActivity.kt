83d0ad5affcec7c659b2bc6d6acbb9ddbcf6d5b5
anlonglong <940752944@qq.com>
2018-07-10 11:01:41 +0800

    添加动态代理的知识点

--- app/src/main/java/com/aillen/spidemo/MainActivity.kt
+++ app/src/main/java/com/aillen/spidemo/MainActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.aillen.aninterface.SayHello
+import java.lang.reflect.Proxy
 import java.util.*
 class MainActivity : AppCompatActivity() {
 
@@ -11,6 +12,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         findViewById<Button>(R.id.btn).setOnClickListener { loadModule() }
+        proxy()
+    }
+
+
+    //动态代理
+    private fun proxy() {
+        val myRun = MyRun()
+        val myInvocationHandler = MyInvocationHandler(myRun)
+        val newProxyInstance = Proxy.newProxyInstance(MyRun::class.java.classLoader, MyRun::class.java.interfaces, myInvocationHandler) as IRun
+        newProxyInstance.run("hello world ------------------------")
     }
 
     private fun loadModule() {
---
+++ app/src/main/java/com/aillen/spidemo/MyInvocationHandler.java
@@ -0,0 +1,25 @@
+package com.aillen.spidemo;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+/**
+ * Created by anlonglong on 2018/7/10.
+ * Email： 940752944@qq.com
+ */
+public class MyInvocationHandler implements InvocationHandler {
+
+    private IRun mIRun;
+
+    public MyInvocationHandler(IRun IRun) {
+        mIRun = IRun;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.println("" +
+                "==========" +
+                "==========");
+        return method.invoke(mIRun,args);
+    }
+}
